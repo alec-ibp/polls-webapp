915c76c35d2543b25d1cf8d8468d3c7af7ed255f
Alejandro <alejo1ibarra@gmail.com>
2022-02-12 21:29:53 -0500

    no hardcoding url on index.html

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,9 +2,12 @@ from . import views
 
 from django.urls import path
 
+
+app_name = "polls"
+
 urlpatterns = [
     path("", views.index, name="Description of the view in this case of index"),
-    path("<int:question_id>/", views.details, name="Question details"),
-    path("<int:question_id>/results", views.results, name="Question results"),
-    path("<int:question_id>/votes", views.votes, name="Question votes")
+    path("<int:question_id>/", views.details, name="details"),
+    path("<int:question_id>/results", views.results, name="results"),
+    path("<int:question_id>/votes", views.votes, name="votes")
     ]
