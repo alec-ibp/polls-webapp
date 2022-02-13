44cceb0e09385fc646587855d9bcb1643cbc048c
Alejandro <alejo1ibarra@gmail.com>
2022-02-12 20:08:42 -0500

    view implement - complete

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -3,5 +3,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="Description of the view in this case of index")
-]
+    path("", views.index, name="Description of the view in this case of index"),
+    path("<int:question_id>/", views.details, name="Question details"),
+    path("<int:question_id>/results", views.results, name="Question results"),
+    path("<int:question_id>/votes", views.votes, name="Question votes")
+    ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello Django")
+
+
+def details(request, question_id):
+    return HttpResponse(f"You are watching the question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"This are the results for the question {question_id}")
+
+
+def votes(request, question_id):
+    return HttpResponse(f"This are the votes for the question {question_id}")
