98139417da600d9e08485291a22fa63498330aa2
Alejandro Ibarra <85713711+ibzen@users.noreply.github.com>
2022-02-19 18:24:00 -0500

    Update models.py

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -17,7 +17,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) # not _id because the orm and automatically -> question_id
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # not _id because the orm add automatically -> question_id
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
